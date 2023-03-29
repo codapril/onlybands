0411c0edfd2abc4e9a253d30de13850bc450b3a2
codapril <129281445+codapril@users.noreply.github.com>
2023-03-29 13:47:27 +0200

    Add profile and settings dummy pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Explore from "./components/Explore";
 import Home from "./components/Home";
 import BandPage from "./components/BandPage";
+import Profile from "./components/Profile";
+import Settings from "./components/Settings"
 
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="explore" element={<Explore />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="settings" element={<Settings />} />
             <Route path="bands/:id" element={<BandPage /> }  />
         </Routes>
       </BrowserRouter>
---
+++ src/components/Profile.js
@@ -0,0 +1,13 @@
+import Layout from "./Layout"
+
+function Profile() {
+
+    return (
+        <Layout>
+            <h3>This will be your profile page</h3>
+        </Layout>
+    )
+    
+  }
+  
+  export default Profile;
